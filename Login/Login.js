f7df1561a2ebb81615a4de1030b7a421f41d6843
Caick Brito <caickgbr2010@gmail.com>
2022-11-15 18:47:21 -0300

    test renamo repositore

--- Login/Login.js
+++ Login/Login.js
@@ -20,23 +20,3 @@ async function entrar() {
 
     alert(resultadojson.mensagem)
 }
-
-async function entrar() {
-    debugger
-    const corpo = {
-        email: document.getElementById("email").value,
-        senha: document.getElementById("senha").value,
-    };
-
-    const requisicao = {
-        method: 'POST',
-        body: JSON.stringify(corpo),
-        heders: {
-            "content-type": "application/json"
-        }
-    }
-    const resultadoApi = await fetch("https://codifica-demo-api.herokuapp.com/api/v1/users/login", requisicao);
-    const resultadojson = await resultadoApi.json();
-
-    alert(resultadojson.mensagem)
-}
